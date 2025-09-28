8ab7d02426b8bfad6897d003b1a6660700aa8f21
Tommaso Chierchia <chierchiatommaso09@gmail.com>
2025-09-28 14:36:45 +0200

    Added LeavesDecay flag

---
+++ src/main/java/it/dominick/regionx/region/settings/impl/LeavesDecaySetting.java
@@ -0,0 +1,37 @@
+package it.dominick.regionx.region.settings.impl;
+
+import it.dominick.regionx.manager.RegionManager;
+import it.dominick.regionx.region.Region;
+import it.dominick.regionx.region.settings.RegionSetting;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.LeavesDecayEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LeavesDecaySetting extends RegionSetting {
+
+    public LeavesDecaySetting(boolean active, JavaPlugin plugin, RegionManager regionManager) {
+        super(active, plugin, regionManager);
+    }
+
+    @Override
+    public String getName() {
+        return "LEAVES_DECAY";
+    }
+
+    @EventHandler
+    public void onLeavesDecay(LeavesDecayEvent event) {
+        if (checkIfInRegion(event.getBlock().getLocation())) {
+            handleEvent(event);
+        }
+    }
+
+    @Override
+    protected void handleEvent(Event event) {
+        LeavesDecayEvent leavesDecayEvent = (LeavesDecayEvent) event;
+        Region region = getRegionManager().getRegion(leavesDecayEvent.getBlock().getLocation());
+
+        triggerAPIEvent(null, event, region);
+        leavesDecayEvent.setCancelled(true);
+    }
+}

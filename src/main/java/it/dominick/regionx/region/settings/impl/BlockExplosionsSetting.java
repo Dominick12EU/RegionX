88bced26483bfcb914138b7b480c08567eb46961
Tommaso Chierchia <chierchiatommaso09@gmail.com>
2025-09-28 13:26:38 +0200

    Added BlockExplosions flag

---
+++ src/main/java/it/dominick/regionx/region/settings/impl/BlockExplosionsSetting.java
@@ -0,0 +1,37 @@
+package it.dominick.regionx.region.settings.impl;
+
+import it.dominick.regionx.manager.RegionManager;
+import it.dominick.regionx.region.Region;
+import it.dominick.regionx.region.settings.RegionSetting;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlockExplosionsSetting extends RegionSetting {
+
+    public BlockExplosionsSetting(boolean active, JavaPlugin plugin, RegionManager regionManager) {
+        super(active, plugin, regionManager);
+    }
+
+    @Override
+    public String getName() {
+        return "BLOCK_EXPLOSIONS";
+    }
+
+    @EventHandler
+    public void onExplodeBlockEvent(BlockExplodeEvent event) {
+        if (checkIfInRegion(event.getBlock().getLocation())) {
+            handleEvent(event);
+        }
+    }
+
+    @Override
+    protected void handleEvent(Event event) {
+        BlockExplodeEvent explodeEvent = (BlockExplodeEvent) event;
+        Region region = getRegionManager().getRegion(((BlockExplodeEvent) event).getBlock().getLocation());
+
+        triggerAPIEvent(null, event, region);
+        explodeEvent.setCancelled(true);
+    }
+}

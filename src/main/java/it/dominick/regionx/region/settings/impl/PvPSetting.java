3807733c72aee4fbf2d90a1bc0471370e327d123
Tommaso Chierchia <chierchiatommaso09@gmail.com>
2025-09-28 13:27:54 +0200

    Added PvP flag

---
+++ src/main/java/it/dominick/regionx/region/settings/impl/PvPSetting.java
@@ -0,0 +1,42 @@
+package it.dominick.regionx.region.settings.impl;
+
+import it.dominick.regionx.manager.RegionManager;
+import it.dominick.regionx.region.Region;
+import it.dominick.regionx.region.settings.RegionSetting;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PvPSetting extends RegionSetting {
+
+    public PvPSetting(boolean active, JavaPlugin plugin, RegionManager regionManager) {
+        super(active, plugin, regionManager);
+    }
+
+    @Override
+    public String getName() {
+        return "PVP";
+    }
+
+    @EventHandler
+    public void onPvP(EntityDamageByEntityEvent event) {
+        if (checkIfInRegion(event.getEntity().getLocation())) {
+            handleEvent(event);
+        }
+    }
+
+    @Override
+    protected void handleEvent(Event event) {
+        EntityDamageByEntityEvent pvpEvent = (EntityDamageByEntityEvent) event;
+        if (pvpEvent.getDamager() instanceof Player && pvpEvent.getEntity() instanceof Player) {
+            Player player = (Player) pvpEvent.getEntity();
+            Region region = getRegionManager().getRegion(player.getLocation());
+
+            triggerAPIEvent(player, event, region);
+            pvpEvent.setCancelled(true);
+        }
+
+    }
+}
